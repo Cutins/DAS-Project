0f4c7f883c1e4a12c3a47a22bec11cc99a642368
Riccardo Paolini <rikipaolini99@gmail.com>
2023-05-25 15:03:58 +0200

    Add files via upload

--- MultisampleNeuralNetwork.py
+++ MultisampleNeuralNetwork.py
@@ -5,8 +5,10 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from PIL import Image
 import os
+import pickle
 
 ###############################################################################
 EPOCHS = 4000
@@ -14,11 +16,29 @@ STEPSIZE = 1e-1
 
 D_NEURONS = 16 # Number of neurons for each layer
 T_LAYERS = 3 # Number of layers
-I_BATCH = 5 # Dimension of the batch set
+BATCHSIZE = 1 # Dimension of the minibatch set
 
-ActivationFunct = "HyTan" # {"Sigmoid", "ReLu", HyTan}
+N_AGENTS = 2
+SEED = 25
+np.random.seed(SEED)
+
+ActivationFunct = "HyTan" # {"Sigmoid", "ReLu", "HyTan"}
 CostFunct = "Quadratic" # {"Quadratic", "BinaryCrossEntropy"}
 
+# Load DataFrame
+file = open('dataset.pkl', 'rb')
+df = pickle.load(file)
+file.close()
+
+# Create Train & Test split
+df_train, df_test = train_test_split(df, test_size=0.1, random_state=SEED, shuffle=True)
+df_train.reset_index(drop=True, inplace=True)
+df_test.reset_index(drop=True, inplace=True)
+
+# Divide data in different sets - one for each agent
+data = {n: df_train.iloc[n::N_AGENTS, :].reset_index(drop=True) for n in range(N_AGENTS)}
+
+N_BATCH = 5#len(df_train) // BATCHSIZE + 1
 ###############################################################################
 # Cost Function
 def cost_fucnt(Y,xT, mask=None):
@@ -30,24 +50,24 @@ def cost_fucnt(Y,xT, mask=None):
     if CostFunct == "BinaryCrossEntropy":
             J = Y*np.log(xT) + (1-Y)*np.log(1-xT)
             dJ = (xT - Y)/(xT*(1-Y))
-    #print(xT - Y)
+
     if CostFunct == "Quadratic":
             J = (xT - Y).T@(xT - Y)
             dJ = 2*(xT - Y)
-    #print(dJ)
+            
     return J, dJ
 
 # Activation Function
 def activation_funct(xl):
 
     if ActivationFunct == "ReLu": # Rectified Linear Unit
-        out = max(0,xl) # xl * (xl>0)
+        out = max(0, xl) # xl * (xl>0)
 
-    if ActivationFunct == "HyTan": # Hyperbolic tangent
+    if ActivationFunct == "HyTan": # Hyperbolic tangent [-1, +1]
         out = np.tanh(xl)
 
-    if ActivationFunct == "Sigmoid": #Sigmoid function
-        out = 1/(1+np.exp(-xl))
+    if ActivationFunct == "Sigmoid": #Sigmoid function [0, +1]
+        out = 1 / (1 + np.exp(-xl))
 
     return out
 
@@ -55,7 +75,7 @@ def activation_funct(xl):
 def activation_funct_derivative(xl):
 
     if ActivationFunct == "ReLu": # Rectified Linear Unit
-        if xl>0:
+        if xl > 0:
             out = 1
         else:
             out = 0
@@ -92,10 +112,10 @@ def forward_pass(x0,uu):
   
 # Adjoint dynamics
 def adjoint_dynamics(ltp, xt, ut):
-    df_dx = np.zeros((D_NEURONS,D_NEURONS))
-    df_du = np.zeros(((D_NEURONS+1)*D_NEURONS,D_NEURONS))
+    df_dx = np.zeros((D_NEURONS, D_NEURONS))
+    df_du = np.zeros(((D_NEURONS+1)*D_NEURONS, D_NEURONS))
 
-    dim = np.tile([D_NEURONS+1],D_NEURONS)
+    dim = np.tile([D_NEURONS+1], D_NEURONS)
     cs_idx = np.append(0, np.cumsum(dim))
   
     for l in range(D_NEURONS):
@@ -135,13 +155,13 @@ def backward_pass(xx,uu,llambdaT):
 CurrentDir = os.path.dirname(os.path.abspath(__file__))
 
 Name = ["000005.jpg", "000007.jpg", "000009.jpg", "000012.jpg", "000022.jpg"]
-#Name = ["000005.jpg", "000007.jpg"]
 
-INPUTS = np.zeros((I_BATCH, D_NEURONS)) # 3 Flattened Images with 16 pixels
-LABEL = np.ones((I_BATCH, D_NEURONS))
-if ActivationFunct == "HyTan": # Hyperbolic tangent
+INPUTS = np.zeros((BATCHSIZE, D_NEURONS)) # 3 Flattened Images with 16 pixels
+LABEL = np.ones((BATCHSIZE, D_NEURONS))
+
+if ActivationFunct == "HyTan":
     LABEL[0,:] = -1 #Le immagini sono tutte corde, la prima è un martello
-if ActivationFunct == "Sigmoid": #Sigmoid function
+if ActivationFunct == "Sigmoid":
     LABEL[0,:] = 0 #Le immagini sono tutte corde, la prima è un martello
 
 for i in range(len(Name)):
@@ -149,12 +169,11 @@ for i in range(len(Name)):
     Input = np.array(Image.open(Path))
     InputNormalized = Input/255.
     INPUTS[i,:] = InputNormalized.reshape(16,)
-    # LABEL[i] = np.mean(INPUTS[i,:]) # Label is mean value of output layer
 
 ############## Plot of images
 # fig, ax = plt.subplots(1, len(Name), figsize=(20,10))
 
-xx = np.zeros((I_BATCH, T_LAYERS, D_NEURONS))
+xx = np.zeros((BATCHSIZE, T_LAYERS, D_NEURONS))
 
 # for j in range(len(Name)):
 #     ax[j].imshow(INPUTS[j,:].reshape(16,1), cmap = "gray", vmin = 0, vmax = 1)
@@ -168,8 +187,8 @@ NormGradientJ = np.zeros(EPOCHS)
 # uu = np.zeros(shape=(T_LAYERS-1,D_NEURONS,D_NEURONS+1)) 
 uu =  np.random.randn(T_LAYERS-1, D_NEURONS, D_NEURONS+1)*1e-1
 
-for i in range(I_BATCH):
-    xx[i] = forward_pass(INPUTS[i],uu)
+# for i in range(BATCHSIZE):
+#     xx[i] = forward_pass(INPUTS[i],uu)
 
 mask = np.zeros(D_NEURONS)
 mask[0] = 1
@@ -179,35 +198,42 @@ for k in range(EPOCHS):
     if k%1000==1:
         print(f'Cost at k={k:d} is {J[k-1]:.4f}')
 
-    delta_u = 0
-    for i in range(I_BATCH):
+    for batch_n in range(N_BATCH):
+        delta_u = 0
+        for batch_sample in range(BATCHSIZE):
+            idx = (batch_n*BATCHSIZE) + batch_sample
+            
+            xx[idx] = forward_pass(INPUTS[idx], uu)
 
-        _, llambdaT = cost_fucnt(LABEL[i],xx[i,-1,:],mask)
-        _, Grad = backward_pass(xx[i],uu,llambdaT)
+            loss, llambdaT = cost_fucnt(LABEL[idx], xx[idx,-1,:], mask)
+            J[k] += loss
+            _, Grad = backward_pass(xx[idx], uu, llambdaT)
 
-        delta_u += Grad/I_BATCH
+            delta_u += Grad / BATCHSIZE
         
-    NormGradientJ[k] = np.linalg.norm(delta_u)
-        
-    uu = uu - STEPSIZE*delta_u
+        uu = uu - STEPSIZE*delta_u 
+        NormGradientJ[k] += np.linalg.norm(delta_u) / N_BATCH
     
-    for i in range(I_BATCH):
-        xx[i] = forward_pass(INPUTS[i],uu)
-        # print(cost_fucnt(LABEL[i],xx[i,-1,:],mask)[0].shape)
-        J[k] += cost_fucnt(LABEL[i],xx[i,-1,:],mask)[0]
-
-    # print(f'Output at k={k:d} is \n {xx[-1,0]}')
-    # print(f'Cost at k={k:d} is {J[k-1]:.4f}')
-
     if k == EPOCHS-1:
-        for i in range(I_BATCH):
-            print(f"Label for Image {i} was {LABEL[i]} but is classified as:", xx[i,-1, 0])
+        for img in range(N_BATCH*BATCHSIZE):
+            print(f"Label for Image {img} was {LABEL[img]} but is classified as:", xx[img,-1, 0])
 
-_, ax = plt.subplots()
-ax.plot(range(EPOCHS),J)
-plt.grid()
 
-_, ax = plt.subplots()
-ax.semilogy(range(EPOCHS), NormGradientJ)
+
+
+
+###############################################################################
+# PLOT
+###############################################################################
+plt.figure('Cost function')
+plt.plot(range(EPOCHS),J)
+plt.title('J')
+
+plt.figure('Norm of Cost function')
+plt.semilogy(range(EPOCHS), NormGradientJ)
+plt.title('norm_gradient_J')
+
 plt.grid()
 plt.show()
+
+
