59fd68581f1e9e820ec0918052232ffda7055f2b
SimoCene <cenerini.simone@gmail.com>
2023-05-25 18:04:04 +0200

    Ordinata cartella git

--- MultisampleNeuralNetwork.py
+++ MultisampleNeuralNetwork.py
@@ -5,40 +5,41 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 from PIL import Image
 import os
 import pickle
 
 ###############################################################################
-EPOCHS = 4000
+EPOCHS = 1000
 STEPSIZE = 1e-1
 
 D_NEURONS = 16 # Number of neurons for each layer
 T_LAYERS = 3 # Number of layers
-BATCHSIZE = 1 # Dimension of the minibatch set
+BATCHSIZE = 16 # Dimension of the minibatch set
 
-# N_AGENTS = 2
-# SEED = 25
-# np.random.seed(SEED)
+N_AGENTS = 1
+SEED = 25
+np.random.seed(SEED)
 
 ActivationFunct = "Sigmoid" # {"Sigmoid", "ReLu", "HyTan"}
 CostFunct = "Quadratic" # {"Quadratic", "BinaryCrossEntropy"}
 
-# # Load DataFrame
-# file = open('dataset.pkl', 'rb')
-# df = pickle.load(file)
-# file.close()
+# Load DataFrame
+file = open('dataset.pkl', 'rb')
+df = pickle.load(file)
+file.close()
 
-# # Create Train & Test split
-# df_train, df_test = train_test_split(df, test_size=0.1, random_state=SEED, shuffle=True)
-# df_train.reset_index(drop=True, inplace=True)
-# df_test.reset_index(drop=True, inplace=True)
+# Create Train & Test split
+df_train, df_test = train_test_split(df, test_size=0.1, random_state=SEED, shuffle=True)
+df_train.reset_index(drop=True, inplace=True)
+df_test.reset_index(drop=True, inplace=True)
 
-# # Divide data in different sets - one for each agent
-# data = {n: df_train.iloc[n::N_AGENTS, :].reset_index(drop=True) for n in range(N_AGENTS)}
+# Divide data in different sets - one for each agent
+data = {n: df_train.iloc[n::N_AGENTS, :].reset_index(drop=True) for n in range(N_AGENTS)}
 
-N_BATCH = 5#len(df_train) // BATCHSIZE + 1
+N_BATCH = len(df_train) // BATCHSIZE + 1
 ###############################################################################
 # Cost Function
 def cost_fucnt(Y,xT, mask=None):
@@ -48,8 +49,7 @@ def cost_fucnt(Y,xT, mask=None):
             xT = xT*mask
 
     if CostFunct == "BinaryCrossEntropy":
-            J = Y*np.log(xT) + (1-Y)*np.log(1-xT)
-            dJ = (xT - Y)/(xT*(1-Y))
+            pass
 
     if CostFunct == "Quadratic":
             J = (xT - Y).T@(xT - Y)
@@ -152,23 +152,29 @@ def backward_pass(xx,uu,llambdaT):
 
 
 #### Load input images from processed folder
-CurrentDir = os.path.dirname(os.path.abspath(__file__))
+#CurrentDir = os.path.dirname(os.path.abspath(__file__))
 
-Name = ["000005.jpg", "000007.jpg", "000009.jpg", "000012.jpg", "000022.jpg"]
+#Name = ["000005.jpg", "000007.jpg", "000009.jpg", "000012.jpg", "000022.jpg"]
 
-INPUTS = np.zeros((N_BATCH, D_NEURONS)) # 3 Flattened Images with 16 pixels
-LABEL = np.ones((N_BATCH, D_NEURONS))
+#INPUTS = np.zeros((BATCHSIZE, D_NEURONS)) # 3 Flattened Images with 16 pixels
+#LABEL = np.ones((BATCHSIZE, D_NEURONS))
 
-if ActivationFunct == "HyTan":
-    LABEL[0,:] = -1 #Le immagini sono tutte corde, la prima è un martello
-if ActivationFunct == "Sigmoid":
-    LABEL[0,:] = 0 #Le immagini sono tutte corde, la prima è un martello
+INPUTS = np.array(df_train['image'].tolist()) # 3 Flattened Images with 16 pixels
+#if ActivationFunct == 'HyTan':
+#    df_train['label'][df_train['label'] == 0] = -1
+LABEL = np.array(df_train['label'].tolist())
+# print(INPUTS[1])
 
-for i in range(N_BATCH):
-    Path = os.path.join(CurrentDir, "FlattenInput", "Flatten" + Name[i])
-    Input = np.array(Image.open(Path))
-    InputNormalized = Input/255.
-    INPUTS[i,:] = InputNormalized.reshape(16,)
+ #if ActivationFunct == "HyTan":
+#    LABEL[0,:] = -1 #Le immagini sono tutte corde, la prima è un martello
+#if ActivationFunct == "Sigmoid":
+#    LABEL[0,:] = 0 #Le immagini sono tutte corde, la prima è un martello
+
+#for i in range(len(Name)):
+#    Path = os.path.join(CurrentDir, "FlattenInput", "Flatten" + Name[i])
+#    Input = np.array(Image.open(Path))
+#    InputNormalized = Input/255.
+#    INPUTS[i,:] = InputNormalized.reshape(16,)
 
 ############## Plot of images
 # fig, ax = plt.subplots(1, len(Name), figsize=(20,10))
@@ -195,18 +201,20 @@ mask[0] = 1
 
 # GO!
 for k in range(EPOCHS):
-    if k%1000==1:
+    if k%10==0 and k!=0:
         print(f'Cost at k={k:d} is {J[k-1]:.4f}')
 
     for batch_n in range(N_BATCH):
         delta_u = 0
         for batch_sample in range(BATCHSIZE):
             idx = (batch_n*BATCHSIZE) + batch_sample
+            if idx >= len(df_train):
+                continue
             
             xx[batch_sample] = forward_pass(INPUTS[idx], uu)
 
             loss, llambdaT = cost_fucnt(LABEL[idx], xx[batch_sample,-1,:], mask)
-            J[k] += loss
+            J[k] += loss / len(df_train)
             _, Grad = backward_pass(xx[batch_sample], uu, llambdaT)
 
             delta_u += Grad / BATCHSIZE
@@ -215,10 +223,8 @@ for k in range(EPOCHS):
         NormGradientJ[k] += np.linalg.norm(delta_u) / N_BATCH
     
     if k == EPOCHS-1:
-        temp = np.zeros((N_BATCH, T_LAYERS, D_NEURONS))
-        for img in range(N_BATCH):
-            temp[img] = forward_pass(INPUTS[img], uu)
-            print(f"Label for Image {img} was {LABEL[img]} but is classified as:", temp[img,-1, 0])
+        for img in range(BATCHSIZE):
+            print(f"Label for Image {img} was {LABEL[img]} but is classified as:", xx[img,-1, 0])
 
 
 
@@ -230,13 +236,12 @@ for k in range(EPOCHS):
 plt.figure('Cost function')
 plt.plot(range(EPOCHS),J)
 plt.title('J')
-plt.grid()
 
 plt.figure('Norm of Cost function')
 plt.semilogy(range(EPOCHS), NormGradientJ)
 plt.title('norm_gradient_J')
-plt.grid()
 
+plt.grid()
 plt.show()
 
 
---
+++ lib/utils.py
@@ -0,0 +1,34 @@
+from tqdm import tqdm
+import os
+import cv2
+import pandas as pd
+import numpy as np
+
+def get_data(dataset_folder, tools, target, size=(224, 224), samples=None, balanced=True):
+    dataset = {'image': [], 'label': []}
+    for tool in tqdm(tools, total=len(tools), leave=False):
+        if tool[0] == '.':
+            continue
+        tool_folder = os.path.join(dataset_folder, tool)
+
+        pos_samples = samples
+        neg_samples = pos_samples // (len(tools) - 1) if balanced else samples
+
+        assert (len(os.listdir(tool_folder)) >= pos_samples if tool == target else len(os.listdir(tool_folder)) >= neg_samples), f"Not enough data for tool {tool} - MAX: {len(os.listdir(tool_folder))}!"
+        
+        for idx, image_name in enumerate(os.listdir(tool_folder)):
+            image_path = os.path.join(tool_folder, image_name)
+            img = cv2.imread(image_path)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.resize(img, size)
+            img = img / 255.
+            img = img.flatten()
+            dataset['image'].append(img)
+            dataset['label'].append(int(tool == target))
+            if samples is not None:
+                if tool == target and idx == pos_samples:
+                    break
+                if tool != target and idx == neg_samples:
+                    break 
+
+    return pd.DataFrame(dataset)
