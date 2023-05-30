5d6631b36bd1719d856748fc9b2a6752572a9feb
SimoCene <cenerini.simone@gmail.com>
2023-05-30 18:34:02 +0200

    Update NeuralNetwork_task_1_2.py
    Risistemata, visto che era stata sovrascritta la volta scorsa

--- NeuralNetwork_task_1_2.py
+++ NeuralNetwork_task_1_2.py
@@ -15,9 +15,6 @@ import pickle
 SEED = 25
 np.random.seed(SEED)
 
-# Select number of agents
-N_AGENTS = 1
-
 # Load DataFrame
 file = open('dataset.pkl', 'rb')
 df = pickle.load(file)
@@ -37,82 +34,30 @@ df_train.reset_index(drop=True, inplace=True)
 df_test.reset_index(drop=True, inplace=True)
 
 # Divide data in different sets - one for each agent
-samples_per_agent = len(df_train) // N_AGENTS
+samples = len(df_train)
 
-images = np.array([df_train['image'][n:N_AGENTS*samples_per_agent:N_AGENTS].tolist() for n in range(N_AGENTS)]) # [N_AGENTS, samples_per_agent, image_size]
-labels = np.array([df_train['label'][n:N_AGENTS*samples_per_agent:N_AGENTS].tolist() for n in range(N_AGENTS)]) # [N_AGENTS, samples_per_agent]
-
-#####################################################################################
-#  Generate Network Graph
-GRAPH_TYPE = "Cycle"    # {"Cycle", "Path", "Star"}
-
-if GRAPH_TYPE == "Cycle":
-	G = nx.path_graph(N_AGENTS)
-	G.add_edge(N_AGENTS-1,0)
-	nx.draw_circular(G, with_labels=True)
-
-if GRAPH_TYPE == "Path":
-	G = nx.path_graph(N_AGENTS)
-	nx.draw(G)
-	
-if GRAPH_TYPE == "Star":
-	G = nx.star_graph(N_AGENTS-1)
-	nx.draw(G, with_labels=True)
-
-plt.show()
-
-ID_AGENTS = np.identity(N_AGENTS, dtype=int)
-
-while 1:
-	ADJ = nx.adjacency_matrix(G)
-	ADJ = ADJ.toarray()	
-
-	test = np.linalg.matrix_power((ID_AGENTS+ADJ),N_AGENTS)
-	
-	if np.all(test>0):
-		print("the graph is connected\n")
-		break 
-	else:
-		print("the graph is NOT connected\n")
-		quit()
-
-
-#####################################################################################
-# METROPOLIS HASTING
-degree = np.sum(ADJ, axis=0)
-WW = np.zeros((N_AGENTS,N_AGENTS))
-
-for ii in range(N_AGENTS):
-   Nii = np.nonzero(ADJ[ii])[0]
-   
-   for jj in Nii:
-      WW[ii,jj] = 1/(1+np.max([degree[ii],degree[jj]]))
-
-   WW[ii,ii] = 1-np.sum(WW[ii,:])
-
-print(WW)
-
-print('Row Stochasticity {}'.format(np.sum(WW,axis=1)))
-print('Col Stochasticity {}'.format(np.sum(WW,axis=0)))
+images = np.array(df_train['image'][:samples]) # [samples, image_size]
+labels = np.array(df_train['label'][:samples]) # [samples]
 
 ###############################################################################
 
 # Cost Function
 def cost_fn(Y,xT, mask=None):
-
-    if mask is not None:
-            Y = Y*mask
-            xT = xT*mask
+    xT0 = xT[0]
 
     if CostFunct == "BinaryCrossEntropy":
-            #J = - Y @ np.log(xT) + (1 - Y) @ np.log(1 - xT)
-            #dJ = (xT - Y) / (xT * (1 - xT))
-            pass
+            J = -(Y*np.log(xT0) + (1-Y)*(np.log(1-xT0)))
+            # dJ = (Y/xT0) + (1-Y)/(1-xT0)
+            dJ = (xT0-Y)/(xT0*(1-xT0)+1e-4) #Preso da cidice di Nicholas
+
 
     if CostFunct == "Quadratic":
-            J = (xT - Y).T@(xT - Y)
-            dJ = 2*(xT - Y)
-            
+            J = (xT0 - Y)*(xT0 - Y)
+            dJ = 2*(xT0 - Y)
+         
+    if mask is not None:
+            dJ = dJ*mask
+
     return J, dJ
 
 
@@ -213,46 +158,42 @@ def backward_pass(xx, uu, llambdaT):
 EPOCHS = 1000
 STEP_SIZE = 1e-1
 BATCH_SIZE = 32 # Dimension of the minibatch set
-N_BATCH = samples_per_agent // BATCH_SIZE + 1
+N_BATCH = np.ceil(samples/BATCH_SIZE)
 
-xx = np.zeros((N_AGENTS, BATCH_SIZE, T_LAYERS, D_NEURONS))
-uu = np.random.randn(N_AGENTS, T_LAYERS-1, D_NEURONS, D_NEURONS+1)*1e-2
-#ss = np.zeros((N_AGENTS, T_LAYERS-1, D_NEURONS, D_NEURONS+1))
+# Network Variables
+xx = np.zeros((BATCH_SIZE, T_LAYERS, D_NEURONS))
+uu = np.random.randn(T_LAYERS-1, D_NEURONS, D_NEURONS+1)*1e-2
+
+# Mask for output regression
 mask = np.zeros(D_NEURONS)
 mask[0] = 1
 
-J = np.zeros(EPOCHS, N_AGENTS) # Cost function
-NormGradientJ = np.zeros(EPOCHS, N_AGENTS)
+J = np.zeros(EPOCHS) # Cost function
+NormGradientJ = np.zeros(EPOCHS)
 
 for epoch in range(EPOCHS):
     if epoch % 10 == 0 and epoch != 0:
         print(f'Cost at k={epoch:d} is {J[epoch-1]:.4f}')
 
     for batch_num in range(N_BATCH):
-        for agent in range(N_AGENTS):
-            neighs = np.nonzero(ADJ[agent])[0]
+        batch_grad = 0
 
-            vv = WW[agent, agent] * uu[agent] 
-            for neigh in neighs:
-                vv += WW[agent, neigh] * uu[neigh]
+        for batch_el in range(BATCH_SIZE):
+            idx = (batch_num*BATCH_SIZE) + batch_el
+            
+            # Skip if samples are finished (last minibatch)
+            if idx >= samples:
+                break
 
-            batch_grad = 0
-            for batch_el in range(BATCH_SIZE):
-                idx = (batch_num*BATCH_SIZE) + batch_el
-                
-                # Skip if samples are finished (last minibatch)
-                if idx >= samples_per_agent:
-                    break
+            xx[batch_el] = forward_pass(images[idx], uu)
+            pred = xx[batch_el, -1, :]
+            loss, out_grad = cost_fn(labels[idx], pred, mask)
+            _, grad = backward_pass(xx[batch_el], uu, out_grad) # out_grad = llambdaT
 
-                xx[agent, batch_el] = forward_pass(images[agent, idx], vv)
-                pred = xx[agent, batch_el, -1, :]
-                loss, out_grad = cost_fn(labels[agent, idx], pred, mask)
-                _, grad = backward_pass(xx[agent, batch_el], vv, out_grad) # out_grad = llambdaT
+            J[epoch] += loss / samples
+            batch_grad += grad / BATCH_SIZE
 
-                J[epoch, agent] += loss / samples_per_agent
-                batch_grad += grad / BATCH_SIZE
-
-            uu[agent] -= STEP_SIZE * batch_grad
-            NormGradientJ[epoch, agent] += np.linalg.norm(batch_grad) / N_BATCH
+        uu = uu - (STEP_SIZE * batch_grad)
+        NormGradientJ[epoch] += np.linalg.norm(batch_grad) / N_BATCH
 
                 
