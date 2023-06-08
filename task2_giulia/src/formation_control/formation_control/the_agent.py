3ae2683f76b0d031d3b7b997ed6273e3ae2cf786
Giulia Cutini <giulia.cutini.gc@gmail.com>
2023-06-08 19:11:05 +0200

    Task2_giulia

---
+++ task2_giulia/src/formation_control/formation_control/the_agent.py
@@ -0,0 +1,134 @@
+import numpy as np
+from time import sleep
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray as MsgFloat
+
+class Agent(Node):
+    def __init__(self):
+        super().__init__('agent',
+                        allow_undeclared_parameters=True,
+                        automatically_declare_parameters_from_overrides=True)
+        
+        self.id = self.get_parameter('agent_id').value
+        self.pos = np.array(self.get_parameter('pos_init').value)
+        self.max_iters = self.get_parameter('max_iters').value
+        self.distances = self.get_parameter('distances').value
+        self.comm_time = self.get_parameter('comm_time').value
+        self.euler_step = self.get_parameter('euler_step').value
+        self.type = self.get_parameter('type').value
+        self.input = self.get_parameter('input').value
+        self.neighs = np.nonzero(self.distances)[0]
+        self.kk = 0
+        self.counter = 0
+        
+        # CREATE TOPIC
+        self.publisher = self.create_publisher(msg_type=MsgFloat, 
+                                                topic=f'/topic_{self.id}',
+                                                qos_profile=10)
+
+        # SUBSCRIBE TO NEIGHBORS
+        for neigh in self.neighs:
+            self.create_subscription(msg_type=MsgFloat,
+                                    topic=f'/topic_{neigh}',
+                                    callback=self.listener_callback,
+                                    qos_profile=10)
+            
+        # CREATE TIMER FOR SYNCH
+        self.timer = self.create_timer(timer_period_sec = self.comm_time,
+                                       callback=self.timer_callback)
+
+        # MASSAGES BUFFER 
+        self.received_msgs = {neigh: [] for neigh in self.neighs}
+
+        print(f"Setup of agent {self.id} completed.")
+
+
+    def listener_callback(self, msg):
+        id, kk, x, y, z = msg.data
+        self.received_msgs[int(id)].append((int(kk), np.array([x, y, z])))
+
+
+    def formation_dynamics(self):
+        delta_pos = np.zeros((3,))
+        for neigh in self.neighs:
+            
+            # Transform data to numpy array
+            neigh_pos = self.received_msgs[neigh].pop(0)[1]
+            neigh_dist = self.distances[neigh]
+
+            # Formation Control Law
+            formation_d_potential = (np.linalg.norm(self.pos - neigh_pos, ord=2)**2 - neigh_dist**2) * (self.pos - neigh_pos)
+            barrier_d_potential = - (2* (self.pos - neigh_pos)/(np.linalg.norm(self.pos - neigh_pos, ord=2)**2))
+
+            delta_pos = delta_pos - (formation_d_potential + barrier_d_potential)
+
+        return delta_pos
+    
+    def leader_dynamics(self):
+        
+
+
+    def timer_callback(self):
+        self.counter += 1
+        if self.kk > 0: 
+
+            all_received = all([self.received_msgs[neigh] != [] for neigh in self.neighs])
+            if all_received:
+                all_synch = all([self.received_msgs[neigh][0][0] == self.kk-1 for neigh in self.neighs])
+                if all_synch:
+
+                    # Formation
+                    delta_pos = self.formation_dynamics()
+
+                    self.pos += self.euler_step * delta_pos
+                    print(f'Counter:\n{self.counter}')
+                    self.counter = 0
+
+                    # Check Termination
+                    if self.kk > self.max_iters:
+
+                        print('\nMAX ITERATIONS')
+                        sleep(3)
+                        self.destroy_node()
+
+                    msg = MsgFloat()
+                    x, y, z = self.pos
+                    msg.data = [float(self.id), float(self.kk), x, y, z]
+                    self.publisher.publish(msg)
+
+                    self.kk += 1
+
+                    self.get_logger().info(f"Agent {int(msg.data[0]):d} -- Iter = {int(msg.data[1]):d}\n\tPosition:\n\t\tx: {msg.data[2]:.4f}\n\t\ty: {msg.data[3]:.4f}\n\t\tz: {msg.data[4]:.4f}")
+
+        else: 
+            msg = MsgFloat()
+            x, y, z = self.pos
+            msg.data = [float(self.id), float(self.kk), x, y, z]
+            self.publisher.publish(msg)
+
+            self.kk += 1
+            self.get_logger().info(f"Agent {int(msg.data[0]):d} -- Iter = {int(msg.data[1]):d}\n\tPosition:\n\t\tx: {msg.data[2]:.4f}\n\t\ty: {msg.data[3]:.4f}\n\t\tz: {msg.data[4]:.4f}")
+        
+
+def main():
+    rclpy.init()
+
+    agent = Agent()
+    agent.get_logger().info(f"Agent {agent.id:d} -- Waiting for sync...")
+    sleep(1)
+    agent.get_logger().info("GO!")
+
+    try:
+        rclpy.spin(agent)
+    except KeyboardInterrupt:
+        agent.get_logger().info("----- Node stopped cleanly -----")
+    finally:
+        rclpy.shutdown() 
+
+
+if __name__ == '__main__':
+    main()   
---
+++ task2_giulia/src/formation_control/formation_control/the_plotter.py
@@ -0,0 +1,138 @@
+import numpy as np
+from time import sleep
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray as MsgFloat
+
+class Plot(Node):
+    def __init__(self):
+        super().__init__('plot',
+                        allow_undeclared_parameters=True,
+                        automatically_declare_parameters_from_overrides=True)
+        
+        self.max_iters = self.get_parameter('max_iters').value
+        # self.n_agents = self.get_parameter('n_agents').value
+        self.n_agents = np.sqrt(len(self.get_parameter('distance_matrix').value)).astype(int)
+        self.distance_matrix = np.array(self.get_parameter('distance_matrix').value).reshape((self.n_agents, self.n_agents))
+        self.comm_time = self.get_parameter('comm_time').value
+        print(f'Distance matrix:\n{self.distance_matrix}')
+        
+        # self.n_agents = self.distance_matrix.shape[0]
+        self.pos = np.zeros((self.max_iters, self.n_agents, 3))
+        self.formation_potential = np.zeros((self.max_iters, self.n_agents))
+        self.barrier_potential = np.zeros((self.max_iters, self.n_agents))
+        self.total_potential = np.zeros((self.max_iters, self.n_agents))
+        self.kk = 0
+        self.test =  np.zeros((self.max_iters, self.n_agents))
+        self.testall = np.zeros((self.max_iters))
+
+        # CREATE TOPIC
+        self.publisher = self.create_publisher(msg_type=MsgFloat, 
+                                                topic=f'/topic_{self.id}',
+                                                qos_profile=10)        
+
+        # SUBSCRIBE TO NEIGHBORS
+        for agent in range(self.n_agents):
+            self.create_subscription(msg_type=MsgFloat,
+                                    topic=f'/topic_{agent}',
+                                    callback=self.listener_callback,
+                                    qos_profile=100)
+            
+        # CREATE TIMER FOR SYNCH
+        self.timer = self.create_timer(timer_period_sec = self.comm_time,
+                                       callback=self.timer_callback)
+
+        # MASSAGES BUFFER 
+        self.received_msgs = {agent: [] for agent in range(self.n_agents)}
+
+        print(f"Setup of plot completed.")
+
+
+    def listener_callback(self, msg):
+        id, kk, x, y, z = msg.data
+        self.received_msgs[int(id)].append((int(kk), np.array([x, y, z])))
+
+    
+    def timer_callback(self):
+        
+        all_received = all([self.received_msgs[agent] != [] for agent in range(self.n_agents)])
+        if all_received: 
+
+            all_synch = all([self.received_msgs[agent][0][0] == self.kk for agent in range(self.n_agents)])
+            if all_synch:       
+                
+                for agent in range(self.n_agents):
+                    self.pos[self.kk, agent] = self.received_msgs[agent].pop(0)[1]
+                for agent in range(self.n_agents):
+                    neighs = np.nonzero(self.distance_matrix[agent])[0]
+                    self.formation_potential[self.kk, agent] = np.sum([1/4*(np.linalg.norm(self.pos[self.kk, agent] - self.pos[self.kk, neigh], ord=2)**2 - self.distance_matrix[agent][neigh]**2)**2 for neigh in neighs])
+                    self.barrier_potential[self.kk, agent] = np.sum([-np.log(np.linalg.norm(self.pos[self.kk, agent] - self.pos[self.kk, neigh], ord=2)**2) for neigh in neighs])
+                    self.total_potential[self.kk, agent] = self.formation_potential[self.kk, agent] + self.barrier_potential[self.kk, agent]
+        
+
+                    # Switch to moving leaders
+                if self.kk > 5:
+                    for agent in range(self.n_agents):
+                        if np.abs(self.formation_potential[self.kk-5, agent]-self.formation_potential[self.kk, agent]) < 0.5e-1:
+                            self.test[self.kk, agent] = 1
+                    if np.all(self.test[self.kk]):
+                        self.testall[self.kk] = 50
+                        print("OKAY...LET'S GO")
+
+                self.kk += 1
+
+                # Check Termination
+                if self.kk == self.max_iters:
+                    print('\nSTART PLOTTING')
+                                            
+                    plt.figure('Formation potential')
+                    for agent in range(self.n_agents):
+                        plt.plot(range(self.max_iters), self.formation_potential[:,agent], ':', label=f'Formation Potential of agent {agent}') 
+                        plt.plot(range(self.max_iters), self.testall[:], label='test', linewidth=5)
+                    plt.plot(range(self.max_iters), np.mean(self.formation_potential, axis=-1), label=f'Total Formation Potential', linewidth = 2)
+                    plt.legend()
+                    plt.grid()
+
+                    plt.figure('Barrier potential')
+                    for agent in range(self.n_agents):
+                        plt.plot(range(self.max_iters), self.barrier_potential[:,agent], ':', label=f'Barrier Potential of agent {agent}') 
+                    plt.plot(range(self.max_iters), np.mean(self.barrier_potential, axis=-1), label=f'Total Barrier Potential', linewidth = 2) 
+                    plt.legend()
+                    plt.grid()
+
+                    plt.figure('Total potential')
+                    for agent in range(self.n_agents):
+                        plt.plot(range(self.max_iters), self.total_potential[:,agent], ':', label=f'Potential of agent {agent}') 
+                    plt.plot(range(self.max_iters), np.mean(self.total_potential, axis=-1), label=f'Total Potential', linewidth = 2) 
+                    plt.legend()
+                    plt.grid()
+
+                    plt.show()
+                        
+                if self.kk >= self.max_iters:
+
+                    print('\nMAX ITERATIONS')
+                    sleep(3)
+                    self.destroy_node()
+
+                
+
+
+def main():
+    rclpy.init()
+
+    plots = Plot()
+
+    try:
+        rclpy.spin(plots)
+    except KeyboardInterrupt:
+        print("----- Node stopped cleanly -----")
+    finally:
+        rclpy.shutdown() 
+
+
+if __name__ == '__main__':
+    main()   
